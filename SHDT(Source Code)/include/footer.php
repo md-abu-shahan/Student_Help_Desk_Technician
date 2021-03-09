36f0a247a00b7491d1ded62f210e5c68f8645a85
Md Abu Shahan <52712036+md-abu-shahan@users.noreply.github.com>
2021-03-09 22:01:08 +0600

    submit

---
+++ SHDT(Source Code)/include/footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+			<div ><a href="https://www.linkedin.com/in/abu-shahan-b52414179/" target="none"><i class="fa fa-linkedin" id="rdownload"></i></a></div>
+			<div><a href="https://www.facebook.com/Md.a.shahan/" target="none"><i class="fa fa-facebook" id="rdownload"></i></a></div>
+			<div><a href="https://myaccount.google.com/" target="none"><i class="fa fa-google" id="rdownload"></i></a></div>
+			<div><a href="https://web.whatsapp.com/" target="none"><i class="fa fa-whatsapp" id="rdownload"></i></a></div>
+			<div><a href="https://www.instagram.com/ahmed_shahan_shah/" target="none"><i class="fa fa-instagram" id="rdownload"></i></a></div>
+			<div><a href="https://www.youtube.com/channel/UCvxQM3EfjEFK-htp5KII6jA" target="none"><i class="fa fa-youtube" id="rdownload"></i></a></div>
+			<div><a href="https://github.com/md-abu-shahan" target="none"><i class="fa fa-github" id="rdownload"></i></a></div>
+			<div><a href="https://www.youtube.com/channel/UCvxQM3EfjEFK-htp5KII6jA" target="none"><i class="fa fa-code" id="rdownload"></i></a></div>
+			<br>
+			<br>
+			<p align="center">Stdudent help desk technician is optimized for course registation and student results. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 2019 by Refsnes Data. All Rights Reserved.
+			Powered by AHN.<br>Copyright © all reserved here</p>
+</div> 
